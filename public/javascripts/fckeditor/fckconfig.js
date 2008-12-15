e40151219c04d571ecb5ddd538e9cf3629b7b0d4
Petrik <petrik@deheus.net>
2008-12-15 17:06:58 +0100

    added radiant skin so link/image popups look more like radiant's admin UI

--- public/javascripts/fckeditor/fckconfig.js
+++ public/javascripts/fckeditor/fckconfig.js
@@ -42,7 +42,7 @@ FCKConfig.StartupShowBlocks = false ;
 FCKConfig.Debug = false ;
 FCKConfig.AllowQueryStringDebug = true ;
 
-FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/default/' ;
+FCKConfig.SkinPath = FCKConfig.BasePath + '../radiant_skin/' ;
 FCKConfig.PreloadImages = [ FCKConfig.SkinPath + 'images/toolbar.start.gif', FCKConfig.SkinPath + 'images/toolbar.buttonarrow.gif' ] ;
 
 FCKConfig.PluginsPath = FCKConfig.BasePath + 'plugins/' ;
