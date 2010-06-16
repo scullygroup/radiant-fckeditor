b8b27dc49a73816a3598b9134a22976dbcad88c8
Brandon Calloway <brandon@scullytown.com>
2010-06-16 10:22:31 -0400

    updated to fix request path for newer Rails versions

--- app/controllers/fckeditor_controller.rb
+++ app/controllers/fckeditor_controller.rb
@@ -236,7 +236,7 @@ class FckeditorController < ApplicationController
   end
   
   def upload_directory_path
-    uploaded = request.relative_url_root.to_s+"#{UPLOADED}/#{params[:Type]}"
+    uploaded = ActionController::Base.relative_url_root.to_s+"#{UPLOADED}/#{params[:Type]}"
     "#{uploaded}#{params[:CurrentFolder]}"
   end
   
